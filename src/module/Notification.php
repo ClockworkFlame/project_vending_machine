8aa3c8a064bc7486ad261cbbcec51082c8728382
Milen I. <milenthrowaway381@gmail.com>
2025-03-14 19:42:00 +0200

    fix

--- src/module/Notification.php
+++ src/module/Notification.php
@@ -1,11 +1,9 @@
 <?php
 namespace Src\Module;
 
-use Src\Interface\Printable;
-
-final class Notification implements Printable
+final class Notification
 {
-    public function __construct(public private(set) readonly string $message, public private(set) readonly string $type) {}
+    public function __construct(public readonly string $message, public readonly string $type) {}
 
     public function print():true {
         echo $message;
